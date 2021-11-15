cf1d78239b661c14df8e340e0d24f5b68569380f
Chester Tan <chester.tan13@imperial.ac.uk>
2021-11-15 06:08:43 +0000

    fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,13 +6,17 @@ docker-compose down -v
 
 sh ./clean.sh
 
-bash ../main.sh pull
+cd ..
 
-cp -r ./frontend/src/build ./mnt/html
+bash ./main.sh pull
+
+cd ./scripts
+
+cp -r ../frontend/src/build ../mnt/html
 
 docker-compose up -d
 
-docker cp -r ../management/src deployment_jupyter_1:/home/jovyan/work
+docker cp ../management/src deployment_jupyter_1:/home/jovyan/work
 
 docker cp ../cues/src/cues.csv deployment_jupyter_1:/home/jovyan/work/export-data/cues.csv
 
