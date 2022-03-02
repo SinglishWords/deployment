c1109e76169ff304b9fd51f9070f8e847070cb17
Shashank Acharya <acharya.s@icloud.com>
2022-03-02 18:49:25 +0800

    Create update_localhost.sh

---
+++ scripts/update_localhost.sh
@@ -0,0 +1,39 @@
+docker-compose up -d
+
+sh data-export.sh
+
+docker-compose down -v
+
+cd ..
+
+# git fetch
+# git reset --hard HEAD
+# git merge '@{u}'
+
+rm -rf ./frontend/src
+rm -rf ./backend/src
+rm -rf ./management/src
+
+git clone https://github.com/SinglishWords/singlish-words-frontend ./frontend/src
+git clone https://github.com/SinglishWords/singlish-words-backend ./backend/src
+git clone https://github.com/SinglishWords/management ./management/src
+
+# useradd swosw -u 1000 -g 1000
+
+# chown -R 1000:1000 *
+
+sysctl net.ipv4.ip_forward
+
+find ./ -type f | xargs sed -i  's/singlishwords[^@]nus.edu.sg(?!.chained)/localhost:443/g'
+
+cp -r ./frontend/src/build/ ./mnt/html
+
+docker-compose build
+
+docker-compose up -d
+
+docker cp ./cues/src/cues.csv deployment_jupyter_1:/home/jovyan/work/export-data/cues.csv
+
+cd ./scripts
+
+sh ./data-refresh.sh
