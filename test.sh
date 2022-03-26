e0d4cf1d9d0aced09085ca72e7e6104bdc71aae9
Shashank Acharya <acharya.s@icloud.com>
2022-03-26 15:46:18 +0800

    removed main and test

--- test.sh
+++
@@ -1,6 +0,0 @@
-base_path="http://localhost:8080"
-
-mkdir reports
-
-ab -l -n 10000 -c 1000 "${base_path}/questions?limit=20" > "reports/questions_limit20.txt"
-ab -l -n 10000 -c 1000 "${base_path}/answers?limit=10" > "reports/answers_limit10.txt"
